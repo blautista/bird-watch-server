a46d552181af4e5c2f4d58dbdd83a8d4d30ed540
blautista <bautistaromanazzi@gmail.com>
2022-02-23 16:46:54 -0300

    Changed AccessControlAllowOrigin to wildcard

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const birdRouter = require("./routes/birdRoutes");
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  // res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 app.use(express.json());
